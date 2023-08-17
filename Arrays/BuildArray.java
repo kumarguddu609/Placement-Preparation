6e7a934a83aaab51bdb03eba7e4044b516921fae
Guddu Kumar <kumarguddu609@gmail.com>
2023-08-17 22:15:55 +0530

    Create BuildArray.java

---
+++ Arrays/BuildArray.java
@@ -0,0 +1,12 @@
+//#1920
+//https://leetcode.com/problems/build-array-from-permutation/
+//Using extra space
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int[] ans=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            ans[i]=nums[nums[i]];
+        }
+        return ans;
+    }
+}
