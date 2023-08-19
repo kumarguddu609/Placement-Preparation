bd1f4b829546c46904ad7dc1f7f6ed0f4ed1200b
Guddu Kumar <kumarguddu609@gmail.com>
2023-08-19 18:22:23 +0530

    Update ConcatenationArray.java
    Advanced for loops

--- Arrays/ConcatenationArray.java
+++ Arrays/ConcatenationArray.java
@@ -16,3 +16,17 @@ class Solution {
 }
 
 //Using Advanced For loop
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int[] ans=new int[2*nums.length];
+        int i=0;
+        for(int element:nums){
+           ans[i]=element;
+           i++;
+        }
+        for(i=nums.length;i<2*nums.length;i++){
+           ans[i]=nums[i-nums.length];
+        }
+        return ans;
+    }
+}
