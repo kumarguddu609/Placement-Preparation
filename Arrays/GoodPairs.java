51c1aa43e6c2aa35cf9a62bdc7dc6802d09866ee
Guddu Kumar <kumarguddu609@gmail.com>
2023-08-20 17:06:19 +0530

    Create GoodPairs.java

---
+++ Arrays/GoodPairs.java
@@ -0,0 +1,16 @@
+//#1512
+//https://leetcode.com/problems/number-of-good-pairs/
+//Using Nested for loops
+
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+           for(int j=i+1;j<nums.length;j++){
+               if(nums[i] == nums[j])
+                    count++;
+           }
+        }
+    return count;      
+    }
+}
