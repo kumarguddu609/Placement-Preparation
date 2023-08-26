ed37d90248ba660a9811952fc59e2e3b5e850edf
Guddu Kumar <kumarguddu609@gmail.com>
2023-08-27 04:06:32 +0530

    Update SmalllerThanCurrent.java
    better solution

--- Arrays/SmalllerThanCurrent.java
+++ Arrays/SmalllerThanCurrent.java
@@ -19,3 +19,23 @@ class Solution {
         return arr;
     }
 }
+
+//Better Solution using bucket array
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] buck= new int[101];
+        //Storing the nums[i] as the index of bucket array
+        for(int i=0;i<nums.length;i++){
+            buck[nums[i]] +=1;
+        }
+        //Adding the previous buck[j-1] to buck[j] to the number smaller or equal to 
+        for(int j=1; j<101; j++){
+            buck[j] += buck[j-1];
+        }
+        for(int k=0;k<nums.length;k++){
+            int pos = nums[k];
+            nums[k] = pos==0?0:buck[pos-1];
+        }
+        return nums;
+    }
+}
