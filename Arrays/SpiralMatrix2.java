4c7b6dc901b9ea59394d9096bddeaeeb0c280b58
Guddu Kumar <kumarguddu609@gmail.com>
2023-08-29 21:36:01 +0530

    Update SpiralMatrix2.java

--- Arrays/SpiralMatrix2.java
+++ Arrays/SpiralMatrix2.java
@@ -45,3 +45,42 @@ class Solution {
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] matrix = new int[n][n];
+        int num = 1;
+        int left = 0, right = n - 1, top = 0, bottom = n - 1;
+
+        while (num <= n * n) {
+            // Traverse from left to right
+            for (int i = left; i <= right; i++) {
+                matrix[top][i] = num++;
+            }
+            top++;
+
+            // Traverse from top to bottom
+            for (int i = top; i <= bottom; i++) {
+                matrix[i][right] = num++;
+            }
+            right--;
+
+            // Traverse from right to left (if necessary)
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    matrix[bottom][i] = num++;
+                }
+                bottom--;
+            }
+
+            // Traverse from bottom to top (if necessary)
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    matrix[i][left] = num++;
+                }
+                left++;
+            }
+        }
+        return matrix;
+    }
+}
+
