a2898d9f4d48d13c4b9a4f32292359a24da9f469
Guddu Kumar <kumarguddu609@gmail.com>
2023-08-30 18:43:01 +0530

    Update GuessNumber.java

--- Searching/GuessNumber.java
+++ Searching/GuessNumber.java
@@ -29,4 +29,27 @@ public class Solution extends GuessGame {
     }
 }
 
-//Better Solution
+//Better Solution using binary search
+
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int start=1; 
+        int end=n; 
+        while(start<=end){
+             int mid=start+(end-start)/2;   
+            int isPick = guess(mid);
+                 if(isPick==-1){
+                    end=mid-1;
+                 }
+                else if(isPick==0){
+                    return mid;
+                }
+                else{
+                 start=mid+1;
+                }
+
+        }
+        return -1;
+    }
+}
