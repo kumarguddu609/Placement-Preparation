e7779d1feff3a9632bf4fd321881734a742e0b0f
Guddu Kumar <kumarguddu609@gmail.com>
2023-08-30 23:04:33 +0530

    Update PerfectSquare.java

--- Searching/PerfectSquare.java
+++ Searching/PerfectSquare.java
@@ -18,3 +18,22 @@ class Solution {
         return false;
     }
 }
+//Takes less space with long datatype
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if(num==1) return true;
+        long start=1, end=num/2;
+        while(start<=end){
+            long mid= start+(end-start)/2;
+            if((long)mid*mid>num){
+                end=mid-1;
+            }
+            else if((long)mid*mid==num){
+                return true;
+            }else{
+                start=mid+1;
+            }
+        }
+        return false;
+    }
+}
