18b01c3f5c3df10180c297b36527887a617eaca3
Guddu Kumar <kumarguddu609@gmail.com>
2023-08-31 17:40:35 +0530

    Update CountNegatives.java

--- Searching/CountNegatives.java
+++ Searching/CountNegatives.java
@@ -15,3 +15,25 @@ class Solution {
         return count;
     }
 }
+//
+//optimised
+class Solution {
+    public int countNegatives(int[][] grid) {
+        int count = 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int row = rows - 1; // Start from the bottom row
+        int col = 0;       // Start from the left-most column
+
+        while (row >= 0 && col < cols) {
+            if (grid[row][col] < 0) {
+                count += cols - col; // All remaining elements in this row are negative
+                row--; // Move to the row above
+            } else {
+                col++; // Move to the next column
+            }
+        }
+
+        return count;
+    }
+}
