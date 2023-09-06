b047f820a37ea44e053005628072b3e8d528549d
Guddu Kumar <kumarguddu609@gmail.com>
2023-09-06 23:52:39 +0530

    Create DoubleExists.java

---
+++ binarySearch/DoubleExists.java
@@ -0,0 +1,15 @@
+#1346
+  //https://leetcode.com/problems/check-if-n-and-its-double-exist/description/
+  //
+  class Solution {
+    public boolean checkIfExist(int[] arr) {
+        for(int i = 0; i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]==2*arr[j] || arr[j]==2*arr[i]){
+                    return true;
+                } 
+            }
+        }
+     return false;   
+    }
+}
