a2922dfe8523fd2a8213ff41a74de71f8cb98023
Guddu Kumar <kumarguddu609@gmail.com>
2023-09-09 23:15:15 +0530

    Update PalindromNumber.java

--- Important/PalindromNumber.java
+++ Important/PalindromNumber.java
@@ -1,3 +1,7 @@
+//#9
+//https://leetcode.com/problems/palindrome-number/description/
+//
+
 class Solution {
     public boolean isPalindrome(int x) {
         if (x<0 || (x!=0 && x%10==0)) return false;
