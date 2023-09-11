26a735bec30cf60f10e883a03188de7c43859024
Guddu Kumar <kumarguddu609@gmail.com>
2023-09-11 20:59:49 +0530

    Update AllMissinigNumber.java

--- Sorting/AllMissinigNumber.java
+++ Sorting/AllMissinigNumber.java
@@ -20,3 +20,32 @@ class Solution {
         return list;
     }
 }
+
+//Using Cyclic Sort
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        ArrayList<Integer> ans=new ArrayList<>();
+        int i=0;
+        while(i<nums.length){
+            int correctIndex=nums[i]-1;
+            if(nums[i]!=nums[correctIndex]){
+                swap(nums,i,correctIndex);
+            }else{
+                i++;
+            }
+        }
+        //missing numbers
+        for(int index=0;index<nums.length;index++){
+            if(nums[index]!=index+1){
+                ans.add(index+1);
+            }
+        }
+        
+        return ans;
+    }
+    void swap(int[] arr, int first, int second){
+        int temp=arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
+    }
+}
