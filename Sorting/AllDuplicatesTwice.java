86f810ac73f19193a90a74b9ceb1e552c125a6c9
Guddu Kumar <kumarguddu609@gmail.com>
2023-09-12 21:38:31 +0530

    Create AllDuplicatesTwice.java

---
+++ Sorting/AllDuplicatesTwice.java
@@ -0,0 +1,23 @@
+//#442
+//
+//
+//
+Using Extra Spaces
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
+        int[] freq = new int[nums.length+1];
+        for(int each : freq){
+            each=0;
+        }
+        for(int i=0;i<nums.length;i++){
+            freq[nums[i]]++;
+        }
+        //searching for twice
+        for(int i=0;i<freq.length;i++){
+            if(freq[i]==2)
+            ans.add(i);
+        }
+        return ans;
+    }
+}
