a6749ccea2c8db44dc4958c29bc3f0a9423921bf
Guddu Kumar <kumarguddu609@gmail.com>
2023-09-15 22:36:31 +0530

    Update MissingPositive.java

--- Sorting/MissingPositive.java
+++ Sorting/MissingPositive.java
@@ -3,46 +3,45 @@
 //
 //
 class Solution {
-  public int firstMissingPositive(int[] nums) {
-    int i = 0;
-    boolean sorted = isSortedArray(nums);
-    if (sorted) {
-      for (int index = 0; index < nums.length; index++) {
-        if (nums[index] != index + 1) {
-          return index + 1;
+    public int firstMissingPositive(int[] nums) {
+    
+        //the first two conditions in the if statement take care 
+        //that negatives and elements greater than the size of 
+        //the array are ignored, while applying cycle sort, and 
+        //rest is the similar code of cycle sort
+
+        int i = 0;
+        while(i < nums.length) {
+            int correct = nums[i] - 1;
+            if(nums[i] <= nums.length && nums[i] > 0 && nums[i] != nums[correct]) {
+
+                int temp = nums[i];
+                nums[i] = nums[correct];
+                nums[correct] = temp;
+            }
+            else {
+                i++;
+            }
         }
-      }
-    }
-    while (i < nums.length) {
-      if (nums[i] <= 0) {
-        i++;
-        continue;
-      }
-      int correct = nums[i] - 1;
-      if (nums[i] != nums[correct]) {
-        swap(nums, i, correct);
-      } else {
-        i++;
-      }
-    }
 
-    for (int index = 0; index < nums.length; index++) {
-      if (nums[index] != index + 1) {
-        return index + 1;
-      }
-    }
-    return -1;
-  }
+        int count = 0;
 
-  boolean isSortedArray(int[] array) {
-    if (array.length == 1 || array.length == 0)
-      return true;
-    for (int i = 1; i < array.length; i++) {
-      if (array[i] < array[i - 1])
-        return false;
+    //after cycle sort has been applied, we will find the first element
+    //which is not at the correct index, and return that index + 1
+
+        for(int j = 0; j < nums.length; j++) {
+
+            if(nums[j] != j + 1) {
+                return j + 1;
+            }
+        }
+
+    //if every element is at its correct index, then obviously the first
+    //missing positive integer is nums.length + 1
+
+        return nums.length+1;
     }
-    return true;
-  }
+}
 
 void swap(int[] arr, int f, int s) {
     int temp = arr[f];
