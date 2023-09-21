25ef1a89d51de86a9c7b56f0c1e23945c1733eb8
Guddu Kumar <kumarguddu609@gmail.com>
2023-09-21 21:53:09 +0530

    Update ValidPalindrome2.java

--- Strings/ValidPalindrome2.java
+++ Strings/ValidPalindrome2.java
@@ -3,6 +3,55 @@
 //
 //
 //
+class Solution {
+    public boolean validPalindrome(String s) {
+        int n = s.length();
+        if(n == 1){
+            return true;
+        }
+        if(isPalin(s))return true;
+        
+        int i = 0;
+        int j = s.length()-1;
+        
+        while(i <= j){
+            if(s.charAt(i) != s.charAt(j)){
+                break;
+            }
+            i++;
+            j--;
+        }
+        StringBuilder sb = new StringBuilder(s);
+        StringBuilder sb2 = new StringBuilder(s);
+        
+        sb.deleteCharAt(i);
+        sb2.deleteCharAt(j);
+        
+        return isPalin(sb.toString()) ||isPalin(sb2.toString());
+
+
+    }
+    
+    private boolean isPalin(String s){
+        
+        int i = 0;
+        int j = s.length()-1;
+        
+        while(i <= j){
+            if(s.charAt(i) != s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
+
+
+
+
+
 //Input size is less
 class Solution {
   public static boolean validPalindrome(String s) {
